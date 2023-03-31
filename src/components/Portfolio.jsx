52173b29bfa22eaac4cda9867200622b7f2dc236
Logan <logancable@ymail.com>
2023-03-30 17:28:03 -0700

    added links to projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,61 @@ function Portfolio() {
     <>
       <Navbar />
       <div className="Portfolio">
-        <p>Portfolio</p>
+        <h1>My Work</h1>
+        <div className="portfolioTop">
+          <div className="projectTop">
+            <h3>Fitness Tracker</h3>
+            <a
+              href="https://fitnesstracker-application.netlify.app/"
+              target="_blank"
+            >
+              <img
+                src="/images/projects/fitnessTracker.png"
+                alt="Fitness Tracker"
+                className="projectImagesTop"
+              />
+            </a>
+          </div>
+          <div className="projectTop">
+            <h3>Stranger's Things</h3>
+            <a
+              href="https://strangersthings-marketplace.netlify.app/"
+              target="_blank"
+            >
+              <img
+                src="/images/projects/stranger'sThings.png"
+                alt="Stranger's Things"
+                className="projectImagesTop"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="portfolioBottom">
+          <div className="project">
+            <h3>Puppy Bowl</h3>
+            <img
+              src="/images/projects/puppyBowl.PNG"
+              alt="Puppy Bowl"
+              className="projectImages"
+            />
+          </div>
+          <div className="project">
+            <h3>Qwirty</h3>
+            <img
+              src="/images/projects/qwirty.png"
+              alt="Qwirty"
+              className="projectImages"
+            />
+          </div>
+          <div className="project">
+            <h3>Tic-Tac-Toe</h3>
+            <img
+              src="/images/projects/ticTacToe.png"
+              alt="Tic-Tac-Toe"
+              className="projectImages"
+            />
+          </div>
+        </div>
         <Link to="/">Go back</Link>
       </div>
       <Footer />
