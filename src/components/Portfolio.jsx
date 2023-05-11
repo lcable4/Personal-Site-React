4cb8c899a43a14415717c697b11e1ec54e82ba7c
Logan <logancable@ymail.com>
2023-05-11 14:59:08 -0700

    added imagelist to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,88 +1,89 @@
-import React from "react";
+import * as React from "react";
+import Card from "@mui/material/Card";
 import { Link } from "react-router-dom";
-import { Navbar, Footer } from "./index";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import ImageListItemBar from "@mui/material/ImageListItemBar";
+import { Navbar } from "./";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+function ProjectCard({ img, title, link, linkName }) {
+  return (
+    <Card sx={{ maxWidth: 500 }}>
+      <CardMedia component="img" height="100" image={img} alt={title} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {linkName && (
+          <Button href={link} target="_blank" size="small">
+            {linkName}
+          </Button>
+        )}
+        {!linkName && (
+          <Button href={link} target="_blank" size="small">
+            Visit
+          </Button>
+        )}
+      </CardActions>
+    </Card>
+  );
+}
 
 function Portfolio() {
   const itemData = [
     {
       img: "/images/projects/DrivEze-lite.png",
       title: "DrivEze Car Rentals",
-
       link: "https://driveze.netlify.app//",
       linkName: "DrivEze",
     },
     {
       img: "/images/projects/fitnessTracker.png",
       title: "Fitness Tracker",
-
       link: "https://fitnesstracker-application.netlify.app/",
     },
     {
       img: "/images/projects/stranger'sThings.png",
       title: "Stranger's Things",
-
       link: "https://strangersthings-marketplace.netlify.app/",
     },
     {
       img: "/images/projects/ticTacToe.png",
       title: "Tic Tac Toe",
-
       link: "https://tictactoe-logancable.netlify.app/",
     },
     {
       img: "/images/projects/mumblr.png",
       title: "Mumblr",
-
       link: "https://github.com/lcable4/Mumblr",
     },
     {
       img: "/images/projects/puppyBowl.png",
       title: "Puppy Bowl",
-
       link: "https://github.com/jina366/puppyBowl/tree/main/src/components",
     },
   ];
+
   return (
     <>
       <Navbar />
       <div className="portfolioContainer">
         <h1 className="portFolioHeaders1">My Work</h1>
-        <ImageList sx={{ width: "100%", height: "100%", objectFit: "contain" }}>
+        <div className="cardContainer">
           {itemData.map((item) => (
-            <ImageListItem className="imgList" key={item.img}>
-              <img
-                src={`${item.img}?w=200&fit=crop&auto=format`}
-                srcSet={`${item.img}?w=200&fit=crop&auto=format&dpr=2 2x`}
-                alt={item.title}
-                loading="lazy"
-                style={{
-                  width: "300px",
-                  height: "300px",
-                  objectFit: "contain",
-                  marginBottom: "20px",
-                  margin: "7px",
-                }}
-              />
-              <a
-                href={item.link}
-                style={{
-                  textDecoration: "none",
-                  color: "blue",
-                  textAlign: "center",
-                  fontSize: "20px",
-                }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <ImageListItemBar title={item.title} position="below" />
-              </a>
-            </ImageListItem>
+            <ProjectCard
+              key={item.title}
+              img={item.img}
+              title={item.title}
+              link={item.link}
+              linkName={item.linkName}
+            />
           ))}
-        </ImageList>
-
+        </div>
         <Link className="goBackLinks" to="/">
           Go back
         </Link>
